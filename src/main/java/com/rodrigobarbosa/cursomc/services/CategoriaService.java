55966eb58ca529db0325ca2af84ca605e3437cf1
Rodrigo Barbosa <rodrigobarbosa1406@gmail.com>
2021-10-08 23:45:29 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/rodrigobarbosa/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.rodrigobarbosa.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rodrigobarbosa.cursomc.domain.Categoria;
+import com.rodrigobarbosa.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+	
+}
